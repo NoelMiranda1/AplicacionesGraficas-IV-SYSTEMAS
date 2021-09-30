ca6e7b29d84d384df8745aac72b86b0b96ef7df1
Noel Miranda <mirandantonionoel@gmail.com>
2021-09-29 22:10:43 -0600

    Search Update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,11 +4,11 @@ import { useState } from "react";
 import { Header } from "../components/header/Header";
 
 export const Home = () => {
-  const [data, setData] = useState<any>([]);
+  const [data, setData] = useState<any>(null);
   console.log("data", data);
   return (
     <div>
-      <Header setData={() => setData([])} />
+      <Header setData={(e: any) => setData(e)} />
     </div>
   );
 };
